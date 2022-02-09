1724be02668fd65fde0cadbd238e830d3e9436dd
l33klin <l33klin@gmail.com>
2022-02-09 14:48:27 +0000

    copy code from github

--- geekbang/ebpf_core_and_practice/lesson08/bcc/execsnoop.py
+++ geekbang/ebpf_core_and_practice/lesson08/bcc/execsnoop.py
@@ -1,20 +1,24 @@
-# 引入库函数
+#!/usr/bin/env python3
+# Tracing execve() system call.
 from bcc import BPF
 from bcc.utils import printb
 
-# 1) 加载eBPF代码
+
+# 1) load BPF program
 b = BPF(src_file="execsnoop.c")
 
-# 2) 输出头
+# 2) print header
 print("%-6s %-16s %-3s %s" % ("PID", "COMM", "RET", "ARGS"))
 
-# 3) 定义性能事件打印函数
+
+# 3) define the callback for perf event
 def print_event(cpu, data, size):
-    # BCC自动根据"struct data_t"生成数据结构
+    # event data struct is generated from "struct data_t" by bcc
     event = b["events"].event(data)
     printb(b"%-6d %-16s %-3d %-16s" % (event.pid, event.comm, event.retval, event.argv))
 
-# 4) 绑定性能事件映射和输出函数，并从映射中循环读取数据
+
+# 4) loop with callback to print_event
 b["events"].open_perf_buffer(print_event)
 while 1:
     try:
